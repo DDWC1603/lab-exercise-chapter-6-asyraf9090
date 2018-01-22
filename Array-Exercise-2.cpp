47c1544e133df9452d51df8f8a5f12afbeb117b7
asyraf9090 <asyraf9090@users.noreply.github.com>
2018-01-22 10:42:34 +0800

    ASYRAF

--- Array-Exercise-2.cpp
+++ Array-Exercise-2.cpp
@@ -1,24 +1,25 @@
 //this is not a complete program. please finish the program. thank you.
-
-
 #include <iostream>
 using namespace std;
 
-int mark[5]={19,10,8,17,9};
+int mark[]={19,10,8,17,9};
 int main()
 {
 	//insert different value to the third element
 	//tukar third element tu dengan value lain
-	mark[]=/*your valie*/
+	cout << mark[2] << endl;
+	mark[2]= 7;
+	cout << "The third element value:"<< mark[2] << endl;
 
 	//get user input and insert sub the third element
 	//tukar the third element yang baru ditukar tu dengan input dari user
-	mark[x];
-
+	cin >> mark[2];
+	cout << "The new third element value:"<< mark[2] << endl;
+	
 	//print the first element of the array
-	mark[];
+	cout << "The first element value"<< mark[0] << endl;
 
 	//print the ith element of the array
-	mark[x]
+	cout << "The last element value:"<<  mark[4] << endl;
 
 }
